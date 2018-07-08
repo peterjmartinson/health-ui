a65c01f85861f4ed18e54a3d210c61173836a15a
Nathan Whitehead <natew@Nathans-MacBook-Air.local>
2018-07-08 18:41:39 -0400

    initial commit for csc8600 health-ui project

---
+++ health-ui/src/app/index/index.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+
+@Component({
+    templateUrl: 'index.component.html'
+})
+export class IndexComponent implements OnInit   {
+
+    display?: string;
+
+    constructor(private router: Router)   { }
+
+    ngOnInit()   {
+        this.display = 'testing index component module';
+    }
+}
